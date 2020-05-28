f0e84d0228a43669257760026e544919ab68c887
Niu Ruslan <niuruslan@gmail.com>
2020-05-28 21:27:28 +0300

    update info

--- src/pages/InfoPage.tsx
+++ src/pages/InfoPage.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import { Card, Col, Row, Button } from "antd";
+import { Card, Col, Row, Button, Anchor } from "antd";
 import { createFromIconfontCN } from "@ant-design/icons";
-
 import "./InfoPage.css";
 
+const { Link } = Anchor;
+
 const IconFont = createFromIconfontCN({
-  scriptUrl: "//at.alicdn.com/t/font_1849843_8uck6j4jaxv.js",
+  scriptUrl: "//at.alicdn.com/t/font_1849843_fillpchkiob.js",
 });
 
 export const InfoPage = () => {
@@ -15,18 +16,17 @@ export const InfoPage = () => {
   return (
     <div className="site-card-wrapper">
       <Row>
-        <Col span={8} offset={8}>
+        <Col md={{ span: 8, offset: 8 }} xs={{ span: 24 }}>
           <Card title="React + TS + Ant Design app">
-            <p>
-              <IconFont type="icon-react" /> React
-            </p>
-            <p>
-              <IconFont type="icon-typescript-def" /> TypeScript
-            </p>
-            <p>
-              <IconFont type="icon-antdesign" /> Ant Design
-            </p>
-            <Button block onClick={() => history.push('/')}>Main Page</Button>
+            <Anchor>
+                <Link href="https://reactjs.org/" target="_blank" title={<><IconFont type="icon-react" /> React </>} />
+                <Link href="https://www.typescriptlang.org/index.html" target="_blank" title={<><IconFont type="icon-typescript-def" /> TypeScript </>} />
+                <Link href="https://ant.design/" target="_blank" title={<><IconFont type="icon-antdesign" /> Ant Design </>} />
+            </Anchor>
+            <br />
+              <Button block onClick={() => history.push("/")}>
+                Main Page
+              </Button>
           </Card>
         </Col>
       </Row>
