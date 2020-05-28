74c1b9dcfb61e674ed1a2d843d1df60ef714bbc6
Niu Ruslan <niuruslan@gmail.com>
2020-05-28 19:56:03 +0300

    Updates

---
+++ precache-manifest.e709c25c20b743012e5c9112a03d708f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8465868b32a010dbc53201b009ccd3b8",
+    "url": "/react-ts-antd/index.html"
+  },
+  {
+    "revision": "172674a4830df3cea5ae",
+    "url": "/react-ts-antd/static/css/2.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "e8657ac769b70bb6633f",
+    "url": "/react-ts-antd/static/css/main.cae4464e.chunk.css"
+  },
+  {
+    "revision": "172674a4830df3cea5ae",
+    "url": "/react-ts-antd/static/js/2.1301fccb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-ts-antd/static/js/2.1301fccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8657ac769b70bb6633f",
+    "url": "/react-ts-antd/static/js/main.2c3ac0f2.chunk.js"
+  },
+  {
+    "revision": "4006ee9adc74025595e8",
+    "url": "/react-ts-antd/static/js/runtime-main.dcd4f7e3.js"
+  }
+]);
