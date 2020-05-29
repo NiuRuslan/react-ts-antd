500da7af82c5111ea127b15e824109540be0c007
Niu Ruslan <niuruslan@gmail.com>
2020-05-29 11:06:18 +0300

    Updates

--- precache-manifest.09465129b7fcb159445582c9c26c534e.js
+++ precache-manifest.09465129b7fcb159445582c9c26c534e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f07a9dda04edd35199e8638cdec05e06",
+    "revision": "c2d38497f0c8451a595bd7bda29b9b1c",
     "url": "/react-ts-antd/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-ts-antd/static/css/2.63b65d4e.chunk.css"
   },
   {
-    "revision": "06e53fdb3f7eef776343",
+    "revision": "850141ea0a63c221f19f",
     "url": "/react-ts-antd/static/css/main.e47a2624.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-ts-antd/static/js/2.1a885c38.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "06e53fdb3f7eef776343",
-    "url": "/react-ts-antd/static/js/main.893eabf7.chunk.js"
+    "revision": "850141ea0a63c221f19f",
+    "url": "/react-ts-antd/static/js/main.6e0cc44d.chunk.js"
   },
   {
     "revision": "4006ee9adc74025595e8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-ts-antd/precache-manifest.ae9d1faa2637bc725f17c8a53336ec0d.js"
+  "/react-ts-antd/precache-manifest.09465129b7fcb159445582c9c26c534e.js"
 );
 
 self.addEventListener('message', (event) => {
